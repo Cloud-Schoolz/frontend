4946d3f88bb73440a1fce66ff8e43b711af248c0
Daniel Gamboa <danielgamboa10@gmail.com>
2021-03-03 16:38:52 -0600

    Add styles to student view

--- cloud-school/src/components/student/VolunteerForStudent.js
+++ cloud-school/src/components/student/VolunteerForStudent.js
@@ -4,10 +4,10 @@ export default function VolunteerForStudent({ volunteer }) {
   const {name, email, country, availability } = volunteer;
 
   return (
-    <div>
+    <div className="volunteer-container">
       <h3>{name}</h3>
-      <p>{email}</p>
-      <p>{country}</p>
+      <a href={`mailto:${email}`} className="volunteer-email">{email}</a>
+      <a href={`http://maps.google.com/?q=${country}`} target="_blank" rel="noreferrer" className="volunteer-country">{country} &#9992;</a>
       <p>Available on {availability}</p>
     </div>
   )
